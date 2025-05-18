0bf23d7b084dfa0138b43956da2cef665bec1727
Bombearo <shekjaden@gmail.com>
2025-05-18 19:29:16 +0100

    Added footer and navbar

---
+++ src/app/components/navBar/navBar.tsx
@@ -0,0 +1,24 @@
+import NavItem from "./navItem";
+import './navBar.css';
+
+function NavBar() {
+    return ( <nav className="navbar">
+        <div className="logo">
+            <img src="/logo.svg" alt="Logo" />
+        </div>
+        <div className="navItems">
+            <NavItem>Home</NavItem>
+            <NavItem>About us</NavItem>
+            <NavItem>Contact</NavItem>
+            <NavItem>Products</NavItem>
+            <NavItem>Learn</NavItem>
+        </div>
+        <div className="buttons">
+            <button className="login">Login</button>
+            <i className="fa-solid fa-cart-shopping"></i>
+        </div>
+            
+        </nav>);
+}
+
+export default NavBar;
