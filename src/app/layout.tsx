e0ed7a6e7502b4f99f67a64f943c22617f84ab53
Bombearo <shekjaden@gmail.com>
2025-05-18 19:58:21 +0100

    Restructured project files to utilise app router better

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
+import NavBar from "../features/components/navBar/navBar";
+import Footer from "../features/components/footer/footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,11 +28,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar />
         {children}
+        <Footer />
       </body>
+      
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 
-import NavBar from "./components/navBar/navBar";
-import Footer from "./components/footer/footer";
-
 export default function Home() {
   return (
+    <main>
     <div className="grid min-h-screen grid-cols-1 grid-rows-[1fr_auto] gap-4 p-4">
-      <NavBar />
-      <main className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-4xl font-bold">Welcome to Wix App</h1>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-4xl font-bold">Welcome to Sanvita</h1>
         <p className="text-lg text-gray-600">
-          This is a simple Next.js app with Tailwind CSS.
+          Slogan goes here
         </p>
-      </main>
-      <Footer />
+      </div>
     </div>
+    <div>
+      About us
+
+    </div>
+    </main>
+    
   );
 }
 
