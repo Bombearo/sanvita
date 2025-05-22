e43a3b30b200daa03df703561e87b156e2e00d76
Bombearo <shekjaden@gmail.com>
2025-05-22 01:26:00 +0100

    Updated section css

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,31 @@
 import "./about.css";
 import Section from "@/features/components/section/section";
+import GridSection from "@/features/components/gridSection/gridSection";
+
+interface ButtonProps{
+    text:string;
+    buttonLink:string;
+}
+
 const aboutSanvita = [
     "Sanvita Health is a leading provider of acupuncture and wellness solutions, dedicated to improving the health and well-being of our clients.",
     "With over 160 years of experience, we are committed to delivering the highest quality products and services.",
 ]
 
 
-const aboutHwaTo = [
-    "Suzhou Medical Appliance Factory, known globally under its brand Hwato, is a leading manufacturer of high-quality acupuncture needles and traditional Chinese medicine (TCM) devices. Founded over 160 years ago, and with a strong focus on safety, precision, and innovation, the company has been setting industry standards since 1980.",
-    
-"As the key organization behind the official Chinese national standard for acupuncture needles (GB2024), Suzhou Medical Appliance Factory has led multiple revisions to ensure quality and consistency. It is also the principal drafter of ISO 17218:2014 — Sterile Acupuncture Needles for Single Use.",
-"By combining decades of expertise with international quality standards (ISO), the company supports the global growth of TCM and offers trusted tools to practitioners worldwide."
+const gridItems = [
+    {
+        title: "Suzhou Medical Appliance Factory",
+        description: "Suzhou Medical Appliance Factory, known globally under its brand Hwato, is a leading manufacturer of high-quality acupuncture needles and traditional Chinese medicine (TCM) devices. Founded over 160 years ago, and with a strong focus on safety, precision, and innovation, the company has been setting industry standards since 1980.",
+    },
+    {
+        title: "Chinese National Standard",
+        description: "As the key organization behind the official Chinese national standard for acupuncture needles (GB2024), Suzhou Medical Appliance Factory has led multiple revisions to ensure quality and consistency. It is also the principal drafter of ISO 17218:2014 — Sterile Acupuncture Needles for Single Use.",
+    },
+    {
+        title: "Global Growth of TCM",
+        description: "By combining decades of expertise with international quality standards (ISO), the company supports the global growth of TCM and offers trusted tools to practitioners worldwide."
+    }
 ]
 export default function About() {
     return (
@@ -18,7 +33,7 @@ export default function About() {
             <main className="">
                 <div className="about-container">
                     <h1 className="main-title">About us</h1>
-                    <h2 className="subheading">One Sentence Description</h2>
+                    <h2 className="subheading">At Sanvita, we are dedicated to providing high-quality acupuncture products and services.</h2>
                     <p className="text-lg text-gray-600">Learn more about us and how we are different!</p>
                 </div>
                 <Section
@@ -28,13 +43,12 @@ export default function About() {
                     buttonLink="/about"
                     reverse={false}
                 />
-                <Section
+                <GridSection
                     title="HwaTo"
-                    content={aboutHwaTo}
-                    buttonText="View Products"
-                    buttonLink="/products"
-                    media={["/images/hwato.jpeg","/images/HQ.png"]}
-                    reverse={true}/>
+                    gridItems={gridItems}
+                    button={{ text: "View Products", buttonLink: "/products" }}
+                    mediaItems={["/images/hwato.jpeg","/images/HQ.png"]}
+                />
             </main>
         </div>
     );
--- src/features/components/gridSection/gridSection.tsx
+++ src/features/components/gridSection/gridSection.tsx
@@ -2,20 +2,28 @@ import GridSectionItem, { GridSectionItemProps } from "./gridSectionItem";
 import Carousel from "../carousel/carousel";
 import "./gridSection.css"
 
-interface GridSectionProps {
-    title: string;
-    mediaItems: string[];
-    gridItems: GridSectionItemProps[];
-    reverse?: boolean;
+interface ButtonProps{
+    text:string;
+    buttonLink:string;
 }
 
-function GridSection({ title,mediaItems, gridItems, reverse }: GridSectionProps) {
+interface GridSectionProps {
+    title: string;
+    mediaItems?: string[];
+    gridItems: GridSectionItemProps[];
+    reverse?: boolean;
+    button?: ButtonProps;
+}
+
+function GridSection({ title,gridItems,mediaItems,  reverse, button }: GridSectionProps) {
     return (
         <div className="grid-section container" style={{ flexDirection: reverse ? 'row-reverse' : 'row' }}>
 
-            <div className="grid-section-image">
-                <Carousel items={mediaItems} />
-            </div>
+            {mediaItems && mediaItems.length > 0 && (
+                <div className="grid-section-image">
+                    <Carousel items={mediaItems} />
+                </div>
+            )}
             <div className="grid-section-content">
                 <h2 className="grid-section-title">{title}</h2>
                 <div className="grid-section-text">
@@ -24,7 +32,15 @@ function GridSection({ title,mediaItems, gridItems, reverse }: GridSectionProps)
                     <GridSectionItem key={index} {...item} />
                 ))}
                 </div>
+                {button && (
+                <div className="grid-section-button">
+                    <a href={button.buttonLink} className="button">
+                        {button.text}
+                    </a>
+                </div>
+            )}
             </div>
+            
             
         </div>
     );
